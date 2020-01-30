dbd6dabeb89ddf387c135101f23b26ee9227d3f0
Shotaro Kobayashi <55833140+Sh0TaR0@users.noreply.github.com>
2020-01-30 10:59:29 +0900

    Update blackjack_dic.py

--- blackjack_dic.py
+++ blackjack_dic.py
@@ -3,19 +3,19 @@ class Deck():
     def __init__(self):
         suits = ['♠', '♥', '♣', '♦']
         numbers = [
-            {"number": "A", "value": 11},
-            {"number": "2", "value": 2},
-            {"number": "3", "value": 3},
-            {"number": "4", "value": 4},
-            {"number": "5", "value": 5},
-            {"number": "6", "value": 6},
-            {"number": "7", "value": 7},
-            {"number": "8", "value": 8},
-            {"number": "9", "value": 9},
-            {"number": "10", "value": 10},
-            {"number": "J", "value": 10},
-            {"number": "Q", "value": 10},
-            {"number": "K", "value": 10}, 
+            {'number': 'A', 'value': 11},
+            {'number': '2', 'value': 2},
+            {'number': '3', 'value': 3},
+            {'number': '4', 'value': 4},
+            {'number': '5', 'value': 5},
+            {'number': '6', 'value': 6},
+            {'number': '7', 'value': 7},
+            {'number': '8', 'value': 8},
+            {'number': '9', 'value': 9},
+            {'number': '10', 'value': 10},
+            {'number': 'J', 'value': 10},
+            {'number': 'Q', 'value': 10},
+            {'number': 'K', 'value': 10},
         ]
 
         self.cards = []
